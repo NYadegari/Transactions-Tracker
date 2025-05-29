9112be7ad1b70fd7bf0ebbb42f1f58030be9dad3
negar <yadegarinegar111@gmail.com>
2025-05-29 17:23:29 +0330

    :triangular_flag_on_post: deleted extra files

--- src/components/CustomCard/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./customcard.module.scss";
-
-const CustomCard = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
-};
-
-export default CustomCard;
